d60e56475a138821a03cd4165def27b82a1a48ab
RomainEconomics <jouhameau.romain@gmail.com>
2021-12-31 11:23:31 +0100

    big update part 2

--- 0_Cleaning_DB.R
+++ 0_Cleaning_DB.R
@@ -2,11 +2,12 @@ library(data.table)
 library(dtplyr)
 library(tidyverse)
 library(dplyr, warn.conflicts = FALSE)
-library(readxl)
 library(janitor) # Pour la fonction clean_names(), ca permet de supprimer les espaces et de mettre les noms de colonnes propores
 library(COVID19)
 library(tsibble)
 library(DBI) # To connect to the database
+
+
 
 #install.packages("RSQLite")
 
@@ -62,6 +63,7 @@ FREDDIE_MAC_Origination_Reduced <- FREDDIE_MAC_Origination2 %>%
 
 FREDDIE_MAC_DB <- FREDDIE_MAC_Performance2 %>% 
   inner_join(FREDDIE_MAC_Origination_Reduced, by = 'Loan_Seq_Number') %>% 
+  collect() %>% 
   drop_na(Seller_Name) 
 
 
@@ -108,11 +110,16 @@ DB <- FREDDIE_MAC_DB %>%
 
 # Cleaning MSA ------------------------------------------------------------
 
-MSA_2020 <- read_excel("Data/MSA_Large.xls", skip = 2) %>% 
+MSA_2020 <- readRDS("Data/MSA_Large.RDS") %>% 
   clean_names() %>% 
   mutate(Concatenated = paste0(fips_state_code, fips_county_code))
 
+read_excel("Data/MSA_Large.xls", skip = 2) %>% 
+  clean_names() %>% 
+  mutate(Concatenated = paste0(fips_state_code, fips_county_code)) %>% 
+  write_rds('Data/MSA_Large.RDS')
 
+readR
 MSA_OK <- DB %>% 
   count(MSA) %>% # 405 MSA
   drop_na(MSA) %>% 
@@ -168,7 +175,6 @@ Final_DB <- DB %>%
 
 
 # Create a SQL like DB ----------------------------------------------------
-
 
 
 con <- dbConnect(RSQLite::SQLite(), "Data/DB.sqlite")
